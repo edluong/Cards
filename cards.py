36501ff9fb6eebbf64a5741eb80b9191d615f273
edluong <edmundluong@gmail.com>
2018-10-09 20:11:59 -0700

    updated str method in card class

--- cards.py
+++ cards.py
@@ -13,13 +13,22 @@ class Card:
     def displayRank(self):
         print(self.rank)
 
+    '''
+        toString function
+        Prints out a human friendly message of what the card.
+        example: King of Hearts if the input was Card('Heart',13)
+    '''
     def __str__(self):
-
-        rankName = {
-            1: 'Ace',
-            11: 'Jack',
-            12: 'Queen',
-            13: 'King'
-        }
-
-        return rankName.get(self.rank,'Invalid Rank') + ' of ' + self.suit + 's'
+        #checks to see if the cards are within the "special" picture cards such as Ace,Jacks, etc...
+        if self.rank in [1,11,12,13]:
+            rankName = {
+                1: 'Ace',
+                11: 'Jack',
+                12: 'Queen',
+                13: 'King'
+            }
+            return rankName.get(self.rank,'Invalid Rank') + ' of ' + self.suit + 's'
+        elif self.rank > 1 and self.rank < 10:
+            return str(self.rank) + ' of ' + self.suit + 's'
+        else:
+            return 'Invalid Card'
